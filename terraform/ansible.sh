6ecd183ed4197495e2917a13c6349465cccbfe0e
tatsuya.shimizu <t.shimizu1115@gmail.com>
2019-08-17 17:57:57 +0900

    detect environments from tfstate.

---
+++ terraform/ansible.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+TF_STATE=. ansible-playbook --private-key=/path/to/ec2user/key \
+--inventory-file=$(which terraform-inventory) \
+--extra-vars="$(terraform show -json ./terraform.tfstate | jq -r '[ .values.root_module.resources[] | (.address | gsub("\\."; "_"))  as $address | .values | keys[] as $k | "\($address)__\($k)='\''\(.[$k])'\''" ] | join(" ")')" \
+../wordpress-ansible/wordpress.yml 
