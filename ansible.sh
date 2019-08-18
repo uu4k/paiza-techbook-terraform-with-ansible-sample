63d9a39260560ff0f7b819cfe93c2aceac93ea05
tatsuya.shimizu <t.shimizu1115@gmail.com>
2019-08-18 12:13:03 +0900

    mv ansible.sh.

--- ansible.sh
+++ ansible.sh
@@ -2,4 +2,4 @@
 TF_STATE=. ansible-playbook --private-key=/path/to/ec2user/key \
 --inventory-file=$(which terraform-inventory) \
 --extra-vars="$(terraform show -json ./terraform.tfstate | jq -r '[ .values.root_module.resources[] | (.address | gsub("\\."; "_"))  as $address | .values | keys[] as $k | "\($address)__\($k)='\''\(.[$k])'\''" ] | join(" ")')" \
-../wordpress-ansible/wordpress.yml 
+wordpress.yml 
